e9cc798487a484570bad606bb62b0f90c6e4762c
Hamidullah Rezae <hamidullahrezae@Hamidullahs-MacBook-Pro.local>
2026-03-14 05:32:57 +1000

    Update fibonacci number algorithm

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+// Fibonacci number using a for loop
+int old_value = 0;
+int older_value = 1;
+int n = 10;
+
+for (int i = 2; i <= n; i++)
+{
+    int newValue = old_value + older_value;
+    Console.WriteLine("F" + i + ": " + newValue);
+    old_value = older_value;
+    older_value = newValue;
+}
+
