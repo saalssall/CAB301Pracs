992f418f581397c847473c716a29d3967881948d
Hamidullah Rezae <hamidullahrezae@Hamidullahs-MacBook-Pro.local>
2026-03-15 05:42:48 +1000

    Add more algorithms for fibonacci

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-
-
-// Fibonacci number using a for loop
+// // Fibonacci number using a for loop
 int old_value = 0;
 int older_value = 1;
 int n = 10;
@@ -13,4 +11,34 @@ for (int i = 2; i <= n; i++)
     old_value = older_value;
     older_value = newValue;
 }
+
+Console.WriteLine("--------------------");
+// Fibonacci number using recursion
+void fibonacci(int prev, int prev1, int count)
+{
+    if (count <= 19)
+    {
+        int new_value = prev + prev1;
+        Console.WriteLine("F" + count + ": " + new_value);
+        fibonacci(prev1, new_value, count + 1);
+    }
+}
+
+fibonacci(0, 1, 2);
+Console.WriteLine("--------------------");
+
+//Another method
+int Fibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+
+Console.WriteLine(Fibonacci(2));
+Console.WriteLine(Fibonacci(3));
+Console.WriteLine(Fibonacci(4));
+Console.WriteLine(Fibonacci(5));
 
