5a2c07acfbf1ccbf539d8194d595986a25bc715d
Hamidullah Rezae <hamidullahrezae@Hamidullahs-MacBook-Pro.local>
2026-03-15 12:04:34 +1000

    Add an algorithm for checking odd or even number

---
+++ Algorithm_Practice/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Algorithm_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Practicing for week1 content
+
+            //1. Checking whether a number in an array is even or odd
+
+            //Input: Array of A[0...n-1] numbers
+            //Output: odd or even number
+
+            // for an Array of size n 
+                //if number is even:
+                    //print(number is even)
+                //else if number is odd:
+                    //print(number is odd)
+                    
+        void checkNum(int num)
+        {
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"{num} is an even number."); 
+            }
+            else if (num % 2 != 0)
+            {
+                Console.WriteLine($"{num} is an odd number.");
+            }
+        }
+        
+        checkNum(100);
+        checkNum(205);
+        checkNum(234);
+        checkNum(633);
+        checkNum(237);
+        checkNum(546);
+        checkNum(135);
+        }
+    }
+}
+
+
+
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f5d1c9e7a1d629d9cf55226163626e93cec531")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+992f418f581397c847473c716a29d3967881948d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
